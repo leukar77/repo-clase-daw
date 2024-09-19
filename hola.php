5dc9cf049cd98117562acece24e3eb0c2585b8f4
raquel <leukar.pinx@gmail.com>
2024-09-19 15:34:16 +0200

    Añadido autor y correo

--- hola.php
+++ hola.php
@@ -1,4 +1,5 @@
 <?php
+	//Realizado por: Raquel Esteve <r.estevesanjuan@edu.gva.es>
 	//El nombre por defecto es "mundo"
 	$nombre=isset($argv[1]) ? $argv[1] : "Mundo";
 	@print  "hola {$nombre}\n";
